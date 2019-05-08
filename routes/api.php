2a8d72a3e7e4cf01f8dd35cb0dc3abdd9236ede2
Eric Kulchycki <erickulchycki@gmail.com>
2019-05-08 15:22:06 -0500

    Clarification on how to hit api.
    Guess I set up some dumb name for localhost
    It's pokedex.test

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//http://pokedex.test/api/----
+
 //List Pokemon
 Route::get('pokemon', 'PokedexController@index');
 
